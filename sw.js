d57c540550892536c6acf085dc997d58c0727986
Bhavik_kanejiya <bhavikkanejiya@gmail.com>
2022-01-01 00:35:37 +0530

    Blog Update V1.4

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211231.2241';
+const cacheName = 'chirpy-20220101.0032';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
