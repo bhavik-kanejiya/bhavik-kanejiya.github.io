36d5a37c2fd276ca9141362039d90056f80788ff
Bhavik_kanejiya <31405066+bhavik-kanejiya@users.noreply.github.com>
2023-08-17 23:30:28 +0530

    Update 1.2

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230818.015404';
+const cacheName = 'chirpy-20230818.015931';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
